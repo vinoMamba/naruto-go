9068bc479866158d2c9bdf6c9f20de8a05c9b5fc
vino <vino0908@outlook.com>
2023-04-24 22:41:45 +0800

    封装 migrate 命令

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,5 +1,58 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file" // required for file:// protocol
+)
 
 var DB *sql.DB
+
+const (
+	host     = "192.168.31.146"
+	port     = 5432
+	user     = "naruto"
+	password = "123456"
+	dbName   = "naruto_dev"
+)
+
+func MigrateUp() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	m, err := migrate.New(
+		fmt.Sprintf("file://%s/config/migrations", dir),
+		fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", user, password, host, port, dbName),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = m.Up()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func MigrateDown() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	m, err := migrate.New(
+		fmt.Sprintf("file://%s/config/migrations", dir),
+		fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", user, password, host, port, dbName),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = m.Steps(-1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
